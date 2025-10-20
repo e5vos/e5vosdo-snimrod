9790332f312eb230874a923a38b91ebf8a6c805b
Nimród Simon <snimrod28@gmail.com>
2025-10-20 07:56:47 +0200

    Increase heartbeat interval from 5 seconds to 20 seconds for reduced load

--- app/api/presentations/sseCapacity/route.ts
+++ app/api/presentations/sseCapacity/route.ts
@@ -78,7 +78,7 @@ if (isTimerWorker) {
   globalState.gSignupPresentationsInterval ??= setInterval(sendCapacity, 2000);
   globalState.gSignupHeartbeatInterval ??= setInterval(
     () => sendCapacity(false),
-    5000,
+    20000,
   );
 }
 
